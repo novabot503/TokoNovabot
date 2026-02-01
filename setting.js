0616149e7e16f6c6b584896bb7cee4533f21d2d4
Novabot <novabot503@gmail.com>
2026-02-02 01:21:14 +0700

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,18 @@
+module.exports = {
+    TELEGRAM_TOKEN: "8332999387:AAEPzYevi0TdcFX7K5GgSlqaPmnrltlVxj0",
+    OWNER_ID: "7587303225",
+
+    VERSI: "1.3",
+    BOT_NAME: "tokonovabot",
+    DEVCELOPER: "@Novabot403",
+    DOMAIN: "https://novabot.rootsec.co.id",
+    PLTA: "ptla_jh2ZcKMgvuOwEg5KGJMPLPBLPbtdHjbfJ60Ae8fH6Gm",
+    EGG: "15",
+    LOX: "1",
+    URLADMIN: "https://t.me/botzmarket95",
+    URLVIDEO: "https://n.uguu.se/VylGjLyr.mp4",
+    PP: "https://h.uguu.se/mWtEjUjt.jpg",
+    PAKASIR_PROJECT: "botzmarket",
+    PAKASIR_API_KEY: "sHli68x22bvfaee3uChutmmbCSz8DaCh",
+    VERCEL: "gY4yPeYsP959NtOJrcTNYOiI"
+};
